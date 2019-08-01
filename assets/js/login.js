0df78040bda7ac3dbbc653e28238b30eeb6c4f20
dixterjake <44016388+dixterjake@users.noreply.github.com>
2019-08-02 00:00:31 +0800

    edited_logn_javascript

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,6 +3,7 @@ function login() {
  var username = document.getElementById("username").value;
  var password = document.getElementById("password").value;
  if(username == 'admin' && password == 'admin'){
+    alert('Welcome! :)');
     window.location.href = "home.html";
  }else{
     alert('Invalid Account');
