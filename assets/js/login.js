db2cafdb5c9a1d62a7160b4fceb7080b112f866f
dixterjake <44016388+dixterjake@users.noreply.github.com>
2019-08-01 23:50:16 +0800

    assingment

---
+++ assets/js/login.js
@@ -0,0 +1,12 @@
+
+function login() {
+ var username = document.getElementById("username").value;
+ var password = document.getElementById("password").value;
+ if(username == 'admin' && password == 'admin'){
+    window.location.href = "home.html";
+ }else{
+    alert('Invalid Account');
+    window.location.href = "index.html";
+ }
+ 
+}
