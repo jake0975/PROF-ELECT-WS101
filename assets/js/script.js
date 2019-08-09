47afcd50d3e611eb5280925959f412eae58ca4bf
jake0975 <madronero.jake0975@gmail.com>
2019-08-09 14:18:31 +0800

    Materialize.css

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-function light(sw) {
-  var pic;
-  if (sw == 0) {
-    pic = "assets/img/off.gif"
-  } else {
-    pic = "assets/img/on.gif"
-  }
-  document.getElementById('myImage').src = pic;
-}
